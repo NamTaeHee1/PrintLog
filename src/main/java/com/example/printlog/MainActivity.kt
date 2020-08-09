da88b423b4dd45837c26a9877f7952422267c95d
Taehee <zmxncbvb123@gmail.com>
2020-08-09 20:07:46 +0900

    PrintLog

---
+++ src/main/java/com/example/printlog/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.printlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    var a : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        a = 2
+
+        if(a == 2) {
+            Log.e("if 문", "ENTER")
+        }
+        else if (a == 1) {
+            Log.e("else if문", "ENTER")
+        }
+
+    }
+}
